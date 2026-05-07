a0b75e6991f4ae653248d6b3dfde6b7c8802ab43
ad4rsh432 <adarshskumar432@gmail.com>
2026-05-07 12:18:53 +0530

    Fix externally-managed-environment error in build script

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
+
 echo "Installing dependencies..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
